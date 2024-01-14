23dc2c6ef2b0e0ddd262266f9663838c4d9aae80
Yep3s <contactosebastianyepes@gmail.com>
2024-01-14 12:36:39 -0500

    Se agrega el modelo del usuario con los getter and setters

---
+++ Taquilla Online/src/Model/Usuario.java
@@ -0,0 +1,52 @@
+package Model;
+
+public class Usuario {
+
+    String name;
+    int dni;
+    String email;
+    String password;
+    int phoneNumber;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getDni() {
+        return dni;
+    }
+
+    public void setDni(int dni) {
+        this.dni = dni;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public int getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(int phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    
+}

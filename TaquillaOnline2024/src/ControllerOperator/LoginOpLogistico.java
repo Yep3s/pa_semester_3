41be15a76de4d84b71f92e715dc948ad574c4850
unknown <contactosebastianyepes@gmail.com>
2024-04-21 13:22:39 -0500

    se agregan todos los archivos del proyecto para guardar (nothing change)

--- TaquillaOnline2024/src/ControllerOperator/LoginOpLogistico.java
+++ TaquillaOnline2024/src/ControllerOperator/LoginOpLogistico.java
@@ -49,33 +49,19 @@ public class LoginOpLogistico {
 
     @FXML
     void regresarAtrasBtn(ActionEvent event) {
-        
-                 try {
-            // Cargar el archivo FXML de la segunda vista
+
+        try {
             FXMLLoader loader = new FXMLLoader(getClass().getResource("/AdminView/0panelStart.fxml"));
             Parent root = loader.load();
-
-            // Obtener el controlador de la segunda vista
-            // Si es necesario realizar acciones en el controlador de la segunda vista, obtén el controlador aquís
-            // Ejemplo: SegundoControlador controller = loader.getController();
-            // Crear una nueva escena y configurarla
             Scene scene = new Scene(root);
-
-            // Crear una nueva ventana
             Stage stage = new Stage();
             stage.setScene(scene);
-            stage.setTitle("Taquilla Online"); // Puedes personalizar el título
-
-            // Mostrar la nueva ventana
+            stage.setTitle("Taquilla Online");
             stage.show();
-
-            // Cerrar la ventana actual (opcional)
             ((Stage) base.getScene().getWindow()).close();
         } catch (Exception e) {
             e.printStackTrace();
         }
-        
-        
 
     }
 
--- TaquillaOnline2024/src/Main/Main.java
+++ TaquillaOnline2024/src/Main/Main.java
@@ -16,20 +16,12 @@ public class Main extends Application {
      @Override
     public void start(Stage primaryStage) {
         try {
-
-            // Cargar el archivo FXML de la primera vista
             FXMLLoader loader = new FXMLLoader(getClass().getResource("/AdminView/0panelStart.fxml"));
             Parent root = loader.load();
-
-            // Configurar el controlador si es necesario
-            // Ejemplo: PanelStart controller = loader.getController();
-            // Crear una nueva escena y configurarla
             Scene scene = new Scene(root);
-
-            // Configurar la escena en la etapa principal
             primaryStage.setScene(scene);
-            primaryStage.setTitle("Taquilla Online"); // Puedes personalizar el título
-            primaryStage.show(); //show es mostrar
+            primaryStage.setTitle("Taquilla Online");
+            primaryStage.show(); 
 
         } catch (Exception e) {
             e.printStackTrace();
